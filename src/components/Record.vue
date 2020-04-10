21c08605ceecbd72b6e0a3077443d57be8c6d303
Tyler Petz <tylerpetz@gmail.com>
2020-04-10 14:36:20 -0600

    move script to to, add hover animation to record in record list

--- src/components/Record.vue
+++ src/components/Record.vue
@@ -1,12 +1,3 @@
-<template>
-  <div class="w-1/6 px-2 mb-6 flex flex-col items-center text-center">
-    <div class="w-full pt-full bg-green-700 relative mb-2">
-      <img :src="record.coverImage" class="block absolute inset-0 object-cover w-full h-full" />
-    </div>
-    <h4 class="text-xs">{{ record.artists[0] }} - {{ record.title }}</h4>
-  </div>
-</template>
-
 <script>
 export default {
   name: 'Record',
@@ -15,3 +6,12 @@ export default {
   }
 }
 </script>
+
+<template>
+  <div v-if="record.coverImage" class="w-1/6 px-2 mb-6 flex flex-col items-center text-center" @click="$emit('itemSelected', record.id)">
+    <div class="w-full pt-full bg-green-700 relative mb-2 shadow-lg transform transition-transform duration-150 scale-100 hover:scale-105 cursor-pointer">
+      <img :src="record.coverImage" class="block absolute inset-0 object-cover w-full h-full" />
+    </div>
+    <h4 class="text-xs">{{ record.artists[0] }} - {{ record.title }}</h4>
+  </div>
+</template>
